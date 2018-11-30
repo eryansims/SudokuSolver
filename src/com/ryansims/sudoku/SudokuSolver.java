713f72e2ae3e0ce5478f8e2dec7967c7bc4e95f5
Michael Sims <michael.h.sims@gmail.com>
2018-11-29 22:09:13 -0600

    Initial commit

---
+++ src/com/ryansims/sudoku/SudokuSolver.java
@@ -0,0 +1,36 @@
+package com.ryansims.sudoku;
+
+/**
+ * This program is a Sudoku Solver. If you get stuck while playing Sudoku, you can
+ * enter you’re puzzle into
+ * the solver and the solver will give you the answers.
+ *
+ * @author Ryan Sims
+ * @version November 16th 2018
+ **/
+public class SudokuSolver {
+
+    private int[][] puzzle;
+
+    private SudokuSolver(int[][] puzzle) {
+        this.puzzle = puzzle;
+    }
+
+    public static void main(String args[]) {
+        new SudokuSolver(new int[][]{
+                {3, 0, 6, 5, 0, 8, 4, 0, 0},
+                {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                {0, 0, 5, 2, 0, 6, 3, 0, 0}
+        }).solve();
+    }
+
+    private void solve() {
+
+    }
+}
